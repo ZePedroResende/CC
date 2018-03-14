b8e12bbb4849fd4061a774e6da172f57c83d129c
ZePedroResende <j.p.resende68@gmail.com>
2018-03-14 12:45:30 +0000

    eu sei que ta no agente mas e monitor

--- Monitor.py
+++ Monitor.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 import socket
 
-MCAST_GRP = '239.8.8.8'
-MCAST_PORT = 8888
-MSG = "Hello World"
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-sock.sendto(MSG.encode(), (MCAST_GRP, MCAST_PORT))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-s.bind(('172.26.71.194', MCAST_PORT))
-while True:
-    msg = sock.recv(1024).decode()
-    print(msg)
+class probe:
+    def __init__(self):
+        self.MCAST_GRP = '239.8.8.8'
+        self.MCAST_PORT = 8888
+        MSG = "Hello World"
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                    socket.IPPROTO_UDP)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+        self.socket.sendto(MSG.encode(), (self.MCAST_GRP, self.MCAST_PORT))
+
+
+class updater:
+    def __init__(self):
+        self.MCAST_PORT = 8888
+        self.s = socket.socket(socket.AF_INET,
+                               socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.s.bind(('172.26.71.194', self.MCAST_PORT))
+        while True:
+            msg = self.s.recv(1024).decode()
+            print(msg)
